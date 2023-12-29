1c2f662742b48f463b6c87a1116885478887041d
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-12-29 23:41:08 +0100

    Added Paystack payment gateway

---
+++ scripts/donate.js
@@ -0,0 +1,31 @@
+
+const nameDonor = document.getElementById('name').value;
+const phone = document.getElementById('phone').value;
+
+const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_262483a6c59b4af13c9e0c085c805a1eaab0a66d', // Replace with your public key
+    email: document.getElementById("email").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+      setTimeout(() => {
+        window.location = 'http://localhost:5500/';
+    }, 3000);
+    }
+  });
+
+  handler.openIframe();
+}
+
